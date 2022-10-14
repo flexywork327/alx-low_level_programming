91825b271e1d0e98c623989b93a3ffcdcfc6c02b
Felix Asare <flexywork327@gmail.com>
2022-10-14 14:25:03 +0000

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
@@ -7,22 +8,7 @@
  */
 int main(void)
 {
-	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	if (n > 5)
-	{
-	printf("and is greater than 5");
-	}
-	if (n == 0)
-	{
-	printf("and is 0");
-	}
-	if (0 >  n < 6)
-	{
-	printf("and is less than 6 and not 0");
-	}
+	putchar("and is greater than \n");
 	return (0);
 }
