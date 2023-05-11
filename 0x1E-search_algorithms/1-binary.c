802530394ab1e3cb82fa967b38ab1ef3502cb80f
Flex <85615389+flexywork327@users.noreply.github.com>
2023-05-11 14:42:09 +0000

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,39 +1,42 @@
 #include "search_algos.h"
 
 /**
-  * binary_search - Searches for a value in a sorted array
-  *                 of integers using binary search.
-  * @array: A pointer to the first element of the array to search.
-  * @size: The number of elements in the array.
-  * @value: The value to search for.
-  *
-  * Return: If the value is not present or the array is NULL, -1.
-  *         Otherwise, the index where the value is located.
-  *
-  * Description: Prints the [sub]array being searched after each change.
-  */
+ * binary_search - search for a value in a sorted array
+ * @array: pointer to the array to search for
+ * @size: size of the array
+ * @value: the target value to search for
+ * Return: the target value index else -1
+ */
+
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i, inital_value, final_value;
+	size_t i, start = 0, end = size - 1;
+	int mid;
 
-	if (!array || array == NULL)
+	if (array == NULL)
 		return (-1);
 
-	for (inital_value = 0, final_value = size - 1; final_value >= inital_value;)
+	while (start <= end)
 	{
-		printf("Searching for value in array: ");
-		for (i = inital_value; i < final_value; i++)
-			printf("%d, ", array[i]);
-		printf("%d\n", array[i]);
+		mid = (start + end) / 2;
 
-		i = inital_value + (final_value - inital_value) / 2;
-		if (array[i] == value)
-			return (i);
-		if (array[i] > value)
-			final_value = i - 1;
+		printf("Searching in array: ");
+		for (i = start; i <= end; i++)
+		{
+			if (i != end)
+				printf("%d, ", array[i]);
+			else
+				printf("%d", array[i]);
+		}
+		printf("\n");
+
+
+		if (array[mid] < value)
+			start =  mid + 1;
+		else if (array[mid] > value)
+			end = mid - 1;
 		else
-			inital_value = i + 1;
+			return (mid);
 	}
-
 	return (-1);
 }
